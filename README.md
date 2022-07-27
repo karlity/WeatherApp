2948f6bf5a1708ba58ab8d85914fa403ab2ebc91
Karli Niemann <karltonrn@gmail.com>
2022-07-26 21:44:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Summary
+This weather app is a few years old and needs to be refactored using the latest Android technologies
+and architecture. 
+
+## Logistics
+Publish your work to your own personal repository (Github, Gitlab, etc)
+  * Provide a link via email to the completed project.
+  * Reach out via email if you have any questions. 
+
+### Show your work
+  * Showing commit history is encouraged.
+
+### Timing
+* We suggest spending 3-5 hours on the exercise. 
+* Please submit your solution no later than 2 weeks after it has been recieved.
+  * If an extension is needed, please let me know via email. 
+
+## Tasks
+* Refactor or rewrite the existing code using best practices.
+  * We try to leverage Jetpack components where possible in Ballpark.
+* The forecast for Wednesday night is not being shown. Add that forecast to the end of the view.
+
+## Guidelines
+* Pull in any external library if necessary.
+* We strongly recommend that you use Kotlin.
+* You have full creative control over the code, views, and structure. Do what makes sense for long
+term maintainability and testability. 
+
+## Expectations
+* The UI should look the same OR better.
+* All forecasts should be shown in the list. 
+
+## Bonus Round
+* Add unit tests
+
+## Screenshot
+![Screenshot](screenshot.png "Screenshot")
---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import android.util.JsonReader
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import java.io.InputStreamReader
+import android.view.View
+import android.widget.ListView
+
+
+class MainActivity : AppCompatActivity() {
+
+    var output = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val obj = this.resources.openRawResource(R.raw.weather)
+        var json = JsonReader(InputStreamReader(obj))
+
+        json.beginObject()
+
+        weatherreader(json)
+        println(output)
+
+        val adapter = ArrayAdapter<String>(this, R.layout.activity_listview, output)
+        val listView: ListView = findViewById<View>(R.id.mobile_list) as ListView
+
+        listView.adapter = adapter
+
+    }
+
+    fun weatherreader(json: JsonReader) {
+        json.hasNext()
+        if (json.nextName() == "periods") {
+            json.beginArray()
+            json.beginObject()
+            parser(json)
+            json.endObject()
+            json.beginObject()
+            parser(json)
+            json.endObject()
+            json.beginObject()
+            parser(json)
+            json.endObject()
+            json.beginObject()
+            parser(json)
+            json.endObject()
+            json.beginObject()
+            parser(json)
+            json.endObject()
+        }
+    }
+
+    fun parser(json: JsonReader) {
+        var tempStore = ""
+        if (json.nextName() == "name") {
+            val value = json.nextString()
+            tempStore += "$value \n"
+        }
+        if (json.nextName() == "temperature") {
+            val value = json.nextInt().toString()
+            tempStore += value
+        }
+        if (json.nextName() == "temperatureUnit") {
+            val value = json.nextString()
+            tempStore += "$value \n"
+        }
+        if (json.nextName() == "windSpeed") {
+            val value = json.nextString()
+            tempStore += "$value "
+        }
+        if (json.nextName() == "windDirection") {
+            val value = json.nextString()
+            tempStore += "$value \n"
+        }
+        if (json.nextName() == "shortForecast") {
+            val value = json.nextString()
+            tempStore += "$value \n"
+        }
+        output.add(tempStore)
+    }
+}
+
+
